011d653a5a2a3b897d8b49396e117bf1d6988931
KarimHamze <kariham9@gmail.com>
2024-08-26 13:13:29 -0300

    create home, register, added form to home, and errors to home

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,6 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def hello(request):
-    return HttpResponse(f'Hello World')
+def home(request):
+    return render(request, 'home.html')
 
 def signup(request):
-    return render(request, 'signup.html', {
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
         'form': UserCreationForm
     })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #Register user
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+                user.save()
+                return HttpResponse('User created sucessfully')
+            except:
+                return render(request, 'signup.html', {
+                    'error': 'Username already exist'
+                 })
+        return render(request, 'signup.html', {
+                    'error': 'Password do not match'
+                })
+
 
