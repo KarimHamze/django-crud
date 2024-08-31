71c4e2b54dc8bcdc85d3a6a4c9192473bc911954
KarimHamze <kariham9@gmail.com>
2024-08-31 14:59:55 -0300

    Boostrap.v2

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,10 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
-        widget = {
-            
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Titulo'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripcion'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'})
         }
